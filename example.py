28439c3df006ad9dc8a37de109f0c9cc9987c3a2
morsisko <morsisko@gmail.com>
2019-05-31 14:46:49 +0200

    Handling HTTP status code

--- example.py
+++ example.py
@@ -5,11 +5,14 @@ router = router.Router(username="admin", password="admin")
 
 while True:
     stats = router.get(timeout=60)
-    print(stats.downstreamCurrRate, "/", stats.downstreamMaxRate)
-    print(stats.upstreamCurrRate, "/", stats.upstreamMaxRate)
-    print(stats.downstreamNoiseMargin, "/", stats.upstreamNoiseMargin)
-    print(stats.downstreamAttenuation, "/", stats.upstreamAttenuation)
-    print(stats.downstreamPower, "/", stats.upstreamPower)
+    if stats.status_code != 200:
+        print("Are you sure your host & password are correct?")
+    else:
+        print(stats.downstreamCurrRate, "/", stats.downstreamMaxRate)
+        print(stats.upstreamCurrRate, "/", stats.upstreamMaxRate)
+        print(stats.downstreamNoiseMargin, "/", stats.upstreamNoiseMargin)
+        print(stats.downstreamAttenuation, "/", stats.upstreamAttenuation)
+        print(stats.downstreamPower, "/", stats.upstreamPower)
 
     print("\n\n")
     time.sleep(30)
